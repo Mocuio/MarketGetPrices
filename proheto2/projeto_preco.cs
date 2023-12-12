6a5800f0d0e36f182c8c6692f4a22d5dfc0308f4
Mocuio <110311564+Mocuio@users.noreply.github.com>
2023-12-12 06:08:38 -0300

    alteração da origem da pasta main

---
+++ proheto2/projeto_preco.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Security.Policy;
+using static System.Net.WebRequestMethods;
+using System.Xml.Linq;
+using System.IO;
+using System.Security.AccessControl;
+using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper;
+using classes;
+using ProjectFunctions;
+
+namespace projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions pg = new Functions();
+           pg.GetDocInfo();
+           pg.GetClientlinks();
+           pg.WriteCsvDocument();
+           
+            //pg.ReadCsvDocument();
+         
+        }
+    }
+}
+
